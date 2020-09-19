6f5fd5dc68c9160282dd4a59146bf1e89f1ab9ca
Thiago Gorgulho Leão <burningbluefox@gmail.com>
2020-09-18 23:16:49 -0300

    (WIP) job for calculating bullet trajectory

---
+++ Assets/Scripts/Gameplay/Jobs/CalculateProjectileTrajectoryJob.cs
@@ -0,0 +1,36 @@
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Jobs;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace BurningBlueFox.Heated.Gameplay
+{
+    [BurstCompile]
+    public struct CalculateProjectileTrajectoryJob : IJobParallelFor
+    {
+        //The world rotation of where the gun barrel points
+        [ReadOnly] public NativeArray<quaternion> muzzleRotation;
+        //Float value representing accuracy, bigger values will result in less precise shot
+        [ReadOnly] public NativeArray<float> projectileDispersion;
+        public NativeArray<Unity.Mathematics.Random> random;
+
+        //Returns the direction and returns it so it only needs to multiply by float speed to get the desired effects
+        public NativeArray<float3> projectileDirection;
+
+
+        public void Execute(int index)
+        {
+            quaternion dispersion = muzzleRotation[index];
+            Unity.Mathematics.Random r = random[index];
+
+            float angle = r.NextFloat(-180f, 180f);
+            float angleCos = projectileDispersion[index] * math.cos(math.radians(angle));
+            float angleSin = projectileDispersion[index] * math.sin(math.radians(angle));
+
+
+            float3 vectorDispersion = new float3(angleCos, angleSin, 0f);
+            projectileDirection[index] = math.normalize(math.forward(dispersion)) + vectorDispersion;
+        }
+    }
+}
--- Assets/Scripts/Input/PlayerFindNearestTargets.cs
+++ Assets/Scripts/Input/PlayerFindNearestTargets.cs
@@ -1,5 +1,7 @@
-﻿using Unity.Collections;
+﻿using BurningBlueFox.Heated.Gameplay;
+using Unity.Collections;
 using Unity.Jobs;
+using Unity.Mathematics;
 using UnityEngine;
 
 namespace BurningBlueFox.Heated.Input
@@ -7,8 +9,16 @@ namespace BurningBlueFox.Heated.Input
     public class PlayerFindNearestTargets : MonoBehaviour
     {
         [SerializeField] Transform reticleTransform = default;
+        [SerializeField] Transform muzzleTransform = default;
         [SerializeField] LayerMask layerMask = default;
+        [SerializeField][Range(0.001f, 1f)] float dispersion = 0.2f;
         private Transform playerTransform;
+
+        private NativeArray<quaternion> muzzleRotation;
+        private NativeArray<float> projectileDispersion;
+        private NativeArray<Unity.Mathematics.Random> random;
+        private NativeArray<float3> output;
+        JobHandle handler;
 
         private void Awake()
         {
@@ -18,10 +28,45 @@ namespace BurningBlueFox.Heated.Input
         private void Update()
         {
             FindNearestTargets();
-
+            SetCalculateBulletTrajectoryJob();
         }
+        private void LateUpdate()
+        {
+            handler.Complete();
+            Debug.DrawRay(muzzleTransform.position, output[0], Color.red);
+        }
+        private void OnEnable()
+        {
+            muzzleRotation = new NativeArray<quaternion>(1, Allocator.Persistent);
+            projectileDispersion = new NativeArray<float>(1, Allocator.Persistent);
+            random = new NativeArray<Unity.Mathematics.Random>(1, Allocator.Persistent);
+            
+            output = new NativeArray<float3>(1, Allocator.Persistent);
+        }
+        private void OnDisable()
+        {
+            muzzleRotation.Dispose();
+            projectileDispersion.Dispose();
+            random.Dispose();
+            output.Dispose();
+        }
+        private void SetCalculateBulletTrajectoryJob()
+        {
+            muzzleRotation[0] = muzzleTransform.rotation;
+            projectileDispersion[0] = dispersion;
+            random[0] = new Unity.Mathematics.Random((uint)UnityEngine.Random.Range(1, 10000));
 
 
+            CalculateProjectileTrajectoryJob job = new CalculateProjectileTrajectoryJob()
+            {
+                muzzleRotation = this.muzzleRotation,
+                projectileDispersion = this.projectileDispersion,
+                random = this.random,
+                projectileDirection = this.output
+            };
+
+            handler = job.Schedule(muzzleRotation.Length, 1);
+        }
         private void FindNearestTargets()
         {
             Vector3 direction = Vector3.forward;
